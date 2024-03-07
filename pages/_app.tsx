4177d3011cc6542e9ca0b56e346b07b1df3db200
zhanghui <zhanghui19880625@outlook.com>
2024-03-06 19:17:31 -0800

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Head from "next/head";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material/styles";
@@ -15,7 +15,7 @@ import { AppState } from "../src/store/Store";
 import NextNProgress from "nextjs-progressbar";
 import BlankLayout from "../src/layouts/blank/BlankLayout";
 import FullLayout from "../src/layouts/full/FullLayout";
-
+import { useRouter } from 'next/router';
 import "../src/_mockApis";
 import "../src/utils/i18n";
 
@@ -46,19 +46,26 @@ const MyApp = (props: MyAppProps) => {
   const theme = ThemeSettings();
   const customizer = useSelector((state: AppState) => state.customizer);
   const Layout = layouts[Component.layout] || FullLayout;
-  const [loading, setLoading] = React.useState(false);
-
-  React.useEffect(() => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const isAuthenticated = useSelector((state: AppState) => state.auth.isAuthenticated);
+  useEffect(() => {
+    // 로딩 시간을 설정하여 앱이 초기화 될 때까지 기다립니다.
     setTimeout(() => setLoading(true), 1000);
-  }, []);
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <title>Modernize NextJs Admin template</title>
-      </Head>
-      <NextNProgress color="#5D87FF" />
-      <ThemeProvider theme={theme}> 
+    // 로그인 상태가 아니라면 로그인 페이지로 리다이렉션
+    if (!isAuthenticated && router.pathname !== '/login') {
+      router.push('/login');
+    }
+  }, [isAuthenticated, router]);
+
+  return ( 
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <title>Modernize NextJs Admin template</title>
+        </Head>
+        <NextNProgress color="#5D87FF" />
+        <ThemeProvider theme={theme}>
           <CssBaseline />
           {loading ? (
             <Layout>
